9e0c67ba81f2e557fcfbb7bde47b5ac0a7013794
israe <israe@Windows11-ISRA>
2024-09-20 21:03:52 +0200

    Exercise5

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println ("Israel De Fuentes");
+		System.out.println ("calle Anton de Alaminos");
+		System.out.println (" ");
+		System.out.println ("641401426");
+	}
+
+}
